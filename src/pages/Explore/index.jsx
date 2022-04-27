193f99629745c9df82afed2dcbc93ab581b59596
Kaike Yudi <kaikeyudi@outlook.com>
2022-04-27 11:13:47 -0300

    tela de exploração finalizada

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header/index';
 
-const Explore = () => (
-  <div>
-    <Header />
-  </div>
-);
+const Explore = () => {
+  const { push } = useHistory();
+
+  const handleRedirect = ({ target: { value } }) => {
+    push(`/explore/${value}`);
+  };
+
+  return (
+    <div>
+      <Header />
+      <button
+        type="button"
+        data-testid="explore-foods"
+        value="foods"
+        onClick={ handleRedirect }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        value="drinks"
+        onClick={ handleRedirect }
+      >
+        Explore Drinks
+      </button>
+    </div>
+  );
+};
 
 export default Explore;
