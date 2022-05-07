5ce3ed6b1680b8261e12fcb0488b0f3ee4ed4bda
Anderson Rodrigues da Silva <omicron_rpl@hotmail.com>
2022-05-06 21:44:10 -0300

    Login page layout template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './routes/index.routes';
 import RecipeProvider from './context/RecipeContext';
+import GlobalStyles from './styles/GlobalStyle';
 
 function App() {
   return (
     <RecipeProvider>
+      <GlobalStyles />
       <Routes />
     </RecipeProvider>
   );
